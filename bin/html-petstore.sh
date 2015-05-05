cee79eca64e25a483b22e2916a4982aadb09226e
Josh Ponelat <josh@ponelat.com>
2015-05-05 18:00:06 +0200

    WIP: fleshing out html some more...

--- bin/html-petstore.sh
+++ bin/html-petstore.sh
@@ -26,6 +26,7 @@ fi
 
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
-ags="$@ generate -i modules/swagger-codegen/src/test/resources/2_0/petstore.json -l html  -o samples/html"
+template_dir="modules/swagger-codegen/src/main/resources/htmlDocs/"
+ags="$@ generate -i modules/swagger-codegen/src/test/resources/2_0/petstore.json -l html -t $template_dir -o samples/html"
 
 java $JAVA_OPTS -jar $executable $ags
